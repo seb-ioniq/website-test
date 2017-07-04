92b1eab7285c672814670f55f43af80e34acf60a
Unknown <sebastien@ics.io>
2017-07-03 17:56:10 -0700

    Test static site src

---
+++ js/nav.es5.js
@@ -0,0 +1,58 @@
+﻿"use strict";
+
+var sections = document.querySelectorAll('section');
+var sectionsOffset = {};
+var i = 0;
+var homeBtn = document.querySelector("#homeBtn");
+var aboutBtn = document.querySelector("#aboutBtn");
+var projectsBtn = document.querySelector("#projectsBtn");
+var contactBtn = document.querySelector("#contactBtn");
+
+[].forEach.call(sections, function (e) {
+    sectionsOffset[e.id] = e.offsetTop;
+});
+homeBtn.addEventListener("click", function (event) {
+    activate(homeBtn);
+});
+
+aboutBtn.addEventListener("click", function (event) {
+    activate(aboutBtn);
+});
+
+projectsBtn.addEventListener("click", function (event) {
+    activate(projectsBtn);
+});
+
+contactBtn.addEventListener("click", function (event) {
+    activate(contactBtn);
+});
+
+window.addEventListener("scroll", function (event) {
+    var scrollPosition = document.documentElement.scrollTop || document.body.scrollTop;
+    for (i in sectionsOffset) {
+        console.log(sectionsOffset[i] + " | " + scrollPosition);
+        if (sectionsOffset[i] <= scrollPosition) {
+            console.log('set!');
+            document.querySelector('.active').setAttribute('class', ' ');
+            document.querySelector('a[href*=' + i + ']').setAttribute('class', 'active');
+        }
+    }
+});
+
+function activate(btn) {
+    var href = btn.getAttribute("href");
+    event.preventDefault();
+    TweenLite.to(window, 1, { scrollTo: { y: href } });
+    removeActive();
+    btn.classList.add("active");
+}
+
+function removeActive() {
+    var btns = document.querySelectorAll("a.active");
+    [].forEach.call(btns, function (el) {
+        el.classList.remove("active");
+    });
+}
+
+console.log(sectionsOffset);
+
---
+++ js/nav.js
@@ -0,0 +1,67 @@
+﻿let sections = document.querySelectorAll('section');
+let sectionsOffset = {};
+let i = 0;
+let currentSection = "home";
+let homeBtn = document.querySelector("#homeBtn");
+let aboutBtn = document.querySelector("#aboutBtn");
+let projectsBtn = document.querySelector("#projectsBtn");
+let contactBtn = document.querySelector("#contactBtn");
+
+[].forEach.call(sections, function (e) {
+    sectionsOffset[e.id] = { top: e.offsetTop, bottom: e.offsetTop + e.offsetHeight };
+});
+
+homeBtn.addEventListener("click", (event) => { 
+    activate(homeBtn);
+});
+
+aboutBtn.addEventListener("click", (event) => {
+    activate(aboutBtn);
+});
+
+projectsBtn.addEventListener("click", (event) => {
+    activate(projectsBtn);
+});
+
+contactBtn.addEventListener("click", (event) => {
+    activate(contactBtn);  
+});
+
+window.addEventListener("load", (event) => {
+    activate(homeBtn);
+});
+
+window.addEventListener("scroll", (event) => {
+    let scrollPosition = document.documentElement.scrollTop || document.body.scrollTop;   
+    checkOffset(scrollPosition);
+});
+
+function activate(btn) {
+    let href = btn.getAttribute("href");
+    event.preventDefault(); 
+    TweenLite.to(window, 1, { scrollTo: { y: href } });
+    removeActive();
+    btn.classList.add("active");
+}
+
+function removeActive() {
+    let btns = document.querySelectorAll("a.active");
+    [].forEach.call(btns, function (el) {
+        el.classList.remove("active");
+    });
+}
+
+function checkOffset(scrollPosition) {
+    for (i in sectionsOffset) {
+        if (scrollPosition >= sectionsOffset[i].top && scrollPosition < sectionsOffset[i].bottom) {
+            history.replaceState("data",
+                "title",
+                location.origin + location.pathname + "#" +
+                i);
+            document.querySelector('.active').setAttribute('class', '');
+            document.querySelector('a[id*=' + i + ']').setAttribute('class', 'active');
+        }
+    }
+}
+
+console.log(sectionsOffset);
